892065e3a4e7fc378741c4aeaed5da36d057026f
UnknownGhost09 <69518728+UnknownGhost09@users.noreply.github.com>
2023-06-05 11:10:50 +0530

    Update models.py

--- trade_bot/core/models.py
+++ trade_bot/core/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
     email_verified_at = models.CharField(max_length=200)
     role = models.CharField(max_length=30, default='3')
     status = models.CharField(max_length=20, default='1')
-    log_id = models.CharField(max_length=200,default=ip.get())
+    #log_id = models.CharField(max_length=200,default=ip.get())
     updated_at = models.CharField(max_length=200,default=datetime.utcnow())
     created_at = models.CharField(max_length=200)
 
